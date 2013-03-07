531acb350816969792466bc7951853d8c66adc24
Eugene Wang <eugene.wang.77@gmail.com>
2013-03-07 17:54:37 -0500

    initiated repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicketTracker2::Application.config.secret_token = '35f68a34c9d0a730d29cbbf2836b9ad2f93e046d809621750a1255cd33ebbefcdf92bb3915e4ecf406c251ea9eabc57c3dbebf4b23817a7d06d111bd61f47a41'
